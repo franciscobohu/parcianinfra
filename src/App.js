6b5052f835e015c6446759b463ed97f315089ee2
= <=>
2019-11-16 10:43:02 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>}
+        < src={logo} cName="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
